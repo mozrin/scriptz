9cf9e212d403483c642a003e115669a90b6f95fa
mozrin <mozrin@gmail.com>
2025-11-04 12:03:10 +0000

    added version.sh to this set

---
+++ src/scripts/version/version.sh
@@ -0,0 +1,13 @@
+version() {
+  echo "lsb_release -a"
+  echo ""
+  lsb_release -a
+  echo ""
+  echo "cat /etc/os-release"
+  echo ""
+  cat /etc/os-release
+  echo ""
+  echo "uname -a"
+  echo ""
+  uname -a
+}
