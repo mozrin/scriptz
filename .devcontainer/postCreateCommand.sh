9ef1090e89ce93e070c097134d598c4bb2c8c2b0
mozrin <mozrin@gmail.com>
2025-11-04 18:54:53 +0700

    updates to devcontainer for scripting

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -16,50 +16,6 @@ apt update
 
 apt install -y \
     iputils-ping \
-    gawk \
-    g++ \
-    make
-
-##### Install C Database Client Libraries
-
-apt install -y \
-    libsqlite3-dev \
-    libmariadb-dev \
-    libpq-dev
-
-##### Conditional PHP Installation
-
-export PHP_INSTALLATION=false
-export PHP_VERSION=8.3
-
-if [ "$PHP_INSTALLATION" = "true" ]; then
-    echo "Installing PHP $PHP_VERSION and Composer..."
-
-    apt install -y \
-        php${PHP_VERSION}-cli \
-        php${PHP_VERSION}-xml \
-        php${PHP_VERSION}-sqlite3 \
-        php${PHP_VERSION}-mysql \
-        php${PHP_VERSION}-redis \
-        php${PHP_VERSION}-memcached \
-        php${PHP_VERSION}-pgsql \
-        php${PHP_VERSION}-pdo-pgsql \
-        php${PHP_VERSION}-mbstring \
-        php${PHP_VERSION}-curl \
-        php${PHP_VERSION}-zip \
-        php${PHP_VERSION}-bcmath \
-        php${PHP_VERSION}-intl \
-        php${PHP_VERSION}-tokenizer \
-        php${PHP_VERSION}-pdo \
-        php${PHP_VERSION}-xdebug \
-        php${PHP_VERSION}-gd
-
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
-    mv composer.phar /usr/local/bin/composer
-else
-    echo "PHP installation skipped."
-fi
+    gawk
 
 ##### Add your changes below here. 
